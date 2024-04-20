3ec0117c1611122296ba3f7b6b15210022346b83
Dirty Old Town <boloks70@gmail.com>
2024-04-20 15:25:31 +0200

    Create update and delete func

--- collection.js
+++ collection.js
@@ -20,6 +20,7 @@ function addItem() {
       album: addAlbum[3].value,
       year: addAlbum[4].value,
     });
+    location.reload();
   }
 }
 
@@ -42,16 +43,87 @@ btnAddAlbum.addEventListener("click", addItem);
             <td>${curRes.value.band}</td>
             <td>${curRes.value.album}</td>
             <td>${curRes.value.year}</td>
-            <td class="icon upd"><i class="fa fa-cog"></i></td>
-            <td class="icon upd"><i class="fa fa-trash"></i></td>
+            <td class="icon upd" onclick="showUpdateButton(${curRes.key})"><i class="fa fa-cog"></i></td>
+            <td class="icon del" onclick="del(${curRes.key})"><i class="fa fa-trash"></i></td>
           </tr>
           `
           curRes.continue();
         } 
+  
       } 
     }
   }
-read()
+// Fill initial record in database
+read();
+
+let primaryKey;
+
+function showUpdateButton(key) {
+  btnAddAlbum.style.display = "none";
+  btnUpdateAlbum.style.display = "block";
+  primaryKey = key;
+
+  // prepare form inputs for update record
+  let request = indexedDB.open("collection", 1);
+  request.onsuccess = function(e) {
+    db = e.target.result;
+    let tx = db.transaction("albums", "readonly");
+    let store = tx.objectStore("albums");
+    let cursor = store.openCursor(primaryKey);
+    cursor.onsuccess = function(e) {
+      let curRes = cursor.result;
+      if (curRes) {
+        addAlbum[0].value = curRes.value.number;
+        addAlbum[1].value = curRes.value.genre;
+        addAlbum[2].value = curRes.value.band;
+        addAlbum[3].value = curRes.value.album;
+        addAlbum[4].value = curRes.value.year;
+        curRes.continue();
+      }
+    }
+  }
+}
+
+function update() {
+  let request = indexedDB.open("collection", 1);
+  request.onsuccess = function(e) {
+    db = e.target.result;
+    let tx = db.transaction("albums", "readwrite");
+    let store = tx.objectStore("albums");
+    if (confirm("About update record, Are you sure?")) {
+      store.put({
+        number: addAlbum[0].value,
+        genre: addAlbum[1].value,
+        band: addAlbum[2].value,
+        album: addAlbum[3].value,
+        year: addAlbum[4].value
+      }, primaryKey);
+      location.reload();
+    } else {
+      return false;
+    }
+  }
+}
+
+btnUpdateAlbum.addEventListener("click", update);
+
+// Delete record
+function del(key) {
+  let request = indexedDB.open("collection", 1);
+  request.onsuccess = function(e) {
+    db = e.target.result;
+    let tx = db.transaction("albums", "readwrite");
+    let store = tx.objectStore("albums");
+    if (confirm("About delete record, Are you sure ?")) {
+      store.delete(key);
+    } else {
+      return false;
+    }
+    location.reload()
+  }
+}
+
+
 
   
 
